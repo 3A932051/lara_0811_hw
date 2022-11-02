cace772408fecc5a2601a8a61b0cf45872c82037
3A932051 <asdf.tw12345@gmail.com>
2022-11-02 11:29:18 +0800

    產生 HomeController, AboutController, 及 NewsController 串接 Controller & 設定 Route, Controller 回傳 View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,13 +15,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home.index');
+
 Route::get('/about', function () {
     return view('welcome');
 })->name('about.index');
+
 Route::get('/news', function () {
     return view('welcome');
-})->name('news.inde
+})->name('news.inde');
